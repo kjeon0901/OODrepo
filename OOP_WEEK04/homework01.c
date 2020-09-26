19c563d060fc9c206f26b4012ab3788d42234963
kjeon0901 <kjeon0901@naver.com>
2020-09-26 18:45:11 +0900

    WEEK04_201902746

---
+++ OOP_WEEK04/homework01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	char s[100];
+	printf("문자를 입력하세요(100자 이내).\n");
+	scanf_s("%[^\n]s", s, sizeof(s));
+	printf("입력된 문자는 %s입니다. \n", s);
+
+	system("pause");
+	return 0;
+}
